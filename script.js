32e3f73ba7d565ec8f7d2a2bcf6dbcc01ddd8dac
Shubham Sharma <112186685+Shubham13th@users.noreply.github.com>
2025-08-21 15:18:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//your JS code here. If required.
+function secondHighest(arr){
+  if(arr.length === 1 || arr.length === 0){
+    return Infinity;
+  }
+
+  arr.sort();
+  let n = arr.length;
+  for(let i=n-1;i>0;i--){
+    if(arr[i] > arr[i-1]){
+      return arr[i-1];
+    }
+  }
+  return Infinity;
+}
+
+console.log(secondHighest(arr));
